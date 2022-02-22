b1a1f4b2f6f7ea5b6a78e5fdb3ff2618b9e18aba
rezartama <91358974+rezartama@users.noreply.github.com>
2022-02-22 12:06:16 +0700

    membuat file LoopingAngka.java

---
+++ LoopingAngka.java
@@ -0,0 +1,13 @@
+package belajardigit;
+
+public class LoopingAngka{
+  
+  public static void main(String[] args){
+   
+    for(int a = 0; a <= 10; a++){
+      System.out.println(a); 
+    }  
+    
+  }                  
+  
+}
