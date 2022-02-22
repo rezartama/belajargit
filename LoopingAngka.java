bdf1c4796f4440e94d3a08f0d7fcfde1c7f01a56
rezartama <91358974+rezartama@users.noreply.github.com>
2022-02-22 12:09:20 +0700

    menambahkan judul pada looping

--- LoopingAngka.java
+++ LoopingAngka.java
@@ -4,6 +4,7 @@ public class LoopingAngka{
   
   public static void main(String[] args){
    
+    System.out.println("LOOPING ANGKA 1 SAMPAI 10");
     for(int a = 0; a <= 10; a++){
       System.out.println(a); 
     }  
